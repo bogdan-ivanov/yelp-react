d7e9c67f54b3e3c624af6dc89ac232ba3157518d
George-Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2016-06-21 20:51:23 +0100

    karma preprocessors

--- karma.conf.js
+++ karma.conf.js
@@ -27,12 +27,14 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
+        'tests.webpack.js'
     ],
 
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
+        'tests.webpack.js': ['webpack', 'sourcemap']
     },
 
 
