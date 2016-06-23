776295c71630eb4c45dc562c3fc085239ebb9a7c
George-Bogdan Ivanov <ivanov.george.bogdan@gmail.com>
2016-06-23 17:11:10 +0100

    putting some console logs

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,18 +1,32 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 import ReactDOM from 'react-dom'
+import { Router } from 'react-router';
 
 import styles from './styles.module.css'
 
-const App = React.createClass({
-  render: function() {
+class App extends React.Component {
+  static propTypes = {
+  	routes: PropTypes.object.isRequired,
+    history: PropTypes.object.isRequired
+  }
+
+  // class getter
+  get content() {
+  	return (<Router routes={this.props.routes} 
+  					history={this.props.history} />)
+  }
+
+  render() {
+  	console.log("=====================")
+  	console.log(this.content)
+  	console.log("=====================")
+  	
     return (
-      <div className={styles.wrapper}>
-        <h1>
-          <i className="fa fa-star"></i>
-          Environment: {__NODE_ENV__}</h1>
-      </div>
+    	<div style={{ height: '100%' }}>
+        	{this.content}
+        </div>
     )
   }
-});
+}
 
 module.exports = App;
